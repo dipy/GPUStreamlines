5d54572bf11d1bd238a3ea4b6ba3d8b2800057aa
Ariel Rokem <arokem@gmail.com>
2020-09-18 21:02:02 -0700

    Adds a script to download HYDI data directly from Figshare.

---
+++ download_hydi.sh
@@ -0,0 +1,5 @@
+wget https://ndownloader.figshare.com/files/18601652 && mv 18601652 brain_mask_1.25.nii.gz
+wget https://ndownloader.figshare.com/files/18601655 && mv 18601655 T1wMPRs003a1001.nii
+wget https://ndownloader.figshare.com/files/18601658 && mv 18601658 HighResHYDIMB3iPAT2APs008a001.bvec
+wget https://ndownloader.figshare.com/files/18601661 && mv 18601661 HighResHYDIMB3iPAT2APs008a001.bval
+wget https://ndownloader.figshare.com/files/18605585 && mv 18605585 BR_eddy_corrected_HYDI_MB3_125_avg_LPCA_1.00_gaussian_SRcollaborative_x2.nii
