10f2fa3426a464c5be8e6d8598ee72f75f7598fa
36000 <jk6.28@outlook.com>
2026-01-06 16:54:47 -0800

    remove todo comment

--- run_gpu_streamlines.py
+++ run_gpu_streamlines.py
@@ -27,7 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import argparse  #  TODO: get this running, commit it, then run clode cleaner
+import argparse
 import random
 import time
 
